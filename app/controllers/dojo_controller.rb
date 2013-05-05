f2490fbaff9a14ba41ed3894f9b0ecde722fa211
Luke Jasko <ltj@andrew.cmu.edu>
2013-05-05 14:21:37 -0400

    Added dojo_controller code for actions

--- app/controllers/dojo_controller.rb
+++ app/controllers/dojo_controller.rb
@@ -1,20 +1,42 @@
 class DojoController < ApplicationController
   def index
+    @dojos = Dojo.active.alphabetical.paginate(:page => params[:page]).per_page(8)
   end
 
   def show
+    @dojo = Dojo.find(params[:id])
+    # --- REVISIT: limit to active students? ---
+    @students = @dojo.students.alphabetical.paginate(:page => params[:page]).per_page(8)
   end
 
   def new
+    @dojo = Dojo.new
   end
 
   def edit
+    @dojo = Dojo.find(params[:id])
   end
 
   def create
+    @dojo = Dojo.new(params[:dojo])
+    
+    if @dojo.save
+      flash[:notice] = "Successfully created #{@dojo.name}"
+      redirect_to @dojo
+    else
+      render :action => 'new'
+    end
   end
 
   def update
+    @dojo = Dojo.find(params[:id])
+    
+    if @dojo.update_attributes(params[:dojo])
+      flash[:notice] = "Successfully updated #{@dojo.name}."
+      redirect_to @dojo
+    else
+      render :action => 'edit'
+    end
   end
 
   def destroy
