473d9453cd1c51e8acf95d909f5aa3b196930a36
Luke Jasko <ltj@andrew.cmu.edu>
2013-05-06 01:14:54 -0400

    Setup basic tournament view with 2 partials. Deleted 'Events' nav link. Setup tournament index page

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,20 +1,44 @@
 class TournamentsController < ApplicationController
   def index
+    # --- REVISIT ---
+    @tournaments = Tournament.active.chronological.paginate(:page => params[:page]).per_page(8)
   end
 
   def show
+    @tournament = Tournament.find(params[:id])
+    temp = @tournament.sections.active.alphabetical
+    @num_sections = temp.size
+    @sections = temp.paginate(:page => params[:page]).per_page(10)
   end
 
   def new
+    @tournament = Tournament.new
   end
 
   def edit
+    @tournament = Tournament.find(params[:id])
   end
 
   def create
+    @tournament = Tournament.new(params[:tournament])
+    
+    if @tournament.save
+      flash[:notice] = "Successfully created #{@tournament.name}"
+      redirect_to @tournament
+    else
+      render :action => 'new'
+    end
   end
 
   def update
+    @tournament = Tournament.find(params[:id])
+    
+    if @tournament.update_attributes(params[:tournament])
+      flash[:notice] = "Successfully updated #{@tournament.name}."
+      redirect_to @tournament
+    else
+      render :action => 'edit'
+    end
   end
 
   def destroy
